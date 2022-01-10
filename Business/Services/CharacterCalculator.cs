2619ee4d5f542449e7b5429b882c6a93fdf23c9b
koganesar <47675029+koganesar@users.noreply.github.com>
2022-01-10 13:43:45 +0300

    logic

---
+++ Business/Services/CharacterCalculator.cs
@@ -0,0 +1,25 @@
+﻿using Business.Models;
+
+namespace Business.Services;
+
+public static class CharacterCalculator
+{
+    public static CalculatedCharacter Calculate(Character character) =>
+        new CalculatedCharacter
+        {
+            Name = character.Name,
+            HitPoints = character.HitPoints,
+            AttackModifier = character.AttackModifier,
+            AttackPerRound = character.AttackPerRound,
+            Damage = character.Damage,
+            DiceType = character.DiceType,
+            DamageModifier = character.DamageModifier,
+            Weapon = character.Weapon,
+            AC = character.AC,
+            MinAcToAlwaysHit = character.AttackModifier + character.Weapon + 1,
+            DamagePerRoundLeft = (character.Damage + character.Weapon + character.DamageModifier) *
+                                 character.AttackPerRound,
+            DamagePerRoundRight = (character.DiceType + character.Weapon + character.DamageModifier) *
+                                  character.AttackPerRound
+        };
+}
---
+++ Business/Services/FightsDealer.cs
@@ -0,0 +1,59 @@
+﻿using System.Text;
+using Business.Controllers;
+using Business.Models;
+
+namespace Business.Services;
+
+public static class FightsDealer
+{
+
+    public static string Fight(Character player, Character monster)
+    {
+        var stringBuilder = new StringBuilder();
+        while (true)
+        {
+            for (var i = 0; i < player.AttackPerRound; i++)
+            {
+                var randomsum = 0;
+                var randomAC = Random.Shared.Next(20) + 1;
+                for (var j = 0; j < player.Damage; j++)
+                {
+                    var random = Random.Shared.Next(player.DiceType) + 1;
+                    randomsum += random;
+                }
+
+                if (monster.AC <= randomAC + player.Weapon + player.AttackModifier)
+                {
+                    monster.HitPoints -= randomsum + player.Weapon + player.DamageModifier;
+                    if (monster.HitPoints <= 0)
+                    {
+                        stringBuilder.Append($"{player.Name} won\n");
+                        return stringBuilder.ToString();
+                    }
+                }
+            }
+
+            for (var i = 0; i < monster.AttackPerRound; i++)
+            {
+                var randomsum = 0;
+                var randomAC = Random.Shared.Next(20) + 1;
+                for (var j = 0; j < monster.Damage; j++)
+                {
+                    var random = Random.Shared.Next(monster.DiceType) + 1;
+                    randomsum += random;
+                }
+
+                if (player.AC <= randomAC + monster.Weapon + monster.AttackModifier)
+                {
+                    player.HitPoints -= randomsum + monster.Weapon + monster.DamageModifier;
+                    if (player.HitPoints <= 0)
+                    {
+                        stringBuilder.Append($"{monster.Name} won");
+                        return stringBuilder.ToString();
+                    }
+                }
+
+            }
+        }
+    }
+}
